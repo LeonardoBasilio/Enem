7550f2ab977c76982b511e0b71880971955cd07c
Leonardo Basilio <leobasilio2010@gmail.com>
2024-05-05 19:57:01 -0300

    Estruturando a árvore de diretórios para expansão do projeto

--- setup.sql
+++ setup.sql
@@ -6,4 +6,14 @@
 --    Nome NVARCHAR(50)
 --);
 
-CREATE DATABASE DockerEnem
+CREATE DATABASE DockerEnem;
+GO
+
+USE DockerEnem;
+
+GO
+
+CREATE TABLE Exemplo (
+    ID INT PRIMARY KEY,
+    Nome NVARCHAR(50)
+);
