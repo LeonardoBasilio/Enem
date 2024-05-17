477fe93e2169f44fb66952bf1b398a68d57c5bca
Leonardo Basilio <leobasilio2010@gmail.com>
2024-05-17 00:30:58 -0300

    ajuste no script

--- import-data.sh
+++ import-data.sh
@@ -1,2 +1,3 @@
 sleep 60
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P bi@123456 -d master -i setup.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P bi@123456 -d master -i setup.sql 
+ 
--- setup.sql
+++ setup.sql
@@ -17,3 +17,10 @@ CREATE TABLE Exemplo (
     ID INT PRIMARY KEY,
     Nome NVARCHAR(50)
 );
+
+
+-- Modifique o tamanho do arquivo de dados primário para 50GB
+ALTER DATABASE [DockerEnem] MODIFY FILE (NAME = 'DockerEnem', SIZE = 50GB);
+
+-- Modifique o tamanho do arquivo de log para 10GB
+ALTER DATABASE [DockerEnem] MODIFY FILE (NAME = 'DockerEnem_log', SIZE = 2GB);
